7fbc7995423d0a162fb664326e21dbc420066f2e
Annirudh Prasad <annirudh@wandb.com>
2020-04-28 13:06:45 -0700

    Fix update labels

--- dataset-versioning/update_all_dataset_labels.py
+++ dataset-versioning/update_all_dataset_labels.py
@@ -1,18 +1,11 @@
 """Create new versions of datasets that need updates."""
 
 import argparse
-import collections
-import json
-import os
-import random
-import shutil
 import sys
-import tempfile
 
 import wandb
 
 import dataset
-import data_library
 
 parser = argparse.ArgumentParser(description='Create new versions of datasets if new labels are available.')
 
@@ -37,19 +30,11 @@ def main(argv):
 
         # construct dataset artifact contents using the example in the loaded dataset,
         # but with the most recent labels from the library.
-        library_ds = dataset.Dataset.from_library_query(
-            ds.example_image_paths,
-            ds_artifact.metadata['annotation_types'])
-        library_ds_artifact = library_ds.artifact()
+        artifact = run.new_artifact(type='dataset', name=d.name, aliases=['latest'])
+        dataset.Dataset.from_library_query(artifact,ds.example_image_paths,
+                                           ds_artifact.metadata['annotation_types'])
 
-        # If the digests aren't equal, then the labels have been updated, save the
-        # library dataset artifact as the new version for this dataset.
-        if ds_artifact.digest != library_ds_artifact.digest:
-            print('  updated, create new dataset version')
-            run.log_artifact(
-                artifact=library_ds_artifact,
-                name=d.name,
-                aliases=['latest'])
+        artifact.save()
 
 
 if __name__ == '__main__':
