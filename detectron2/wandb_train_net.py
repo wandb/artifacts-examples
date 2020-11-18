eff75890176ccab4400541ee74390539f71f578e
Tim Sweeney <tim.s.sweeney@gmail.com>
2020-11-17 22:05:43 -0800

    new and improved

--- detectron2/wandb_train_net.py
+++ detectron2/wandb_train_net.py
@@ -277,6 +277,7 @@ def main(args):
                         'box_data': boxes
                     }
                 })
+            import pdb; pdb.set_trace()
             table.add_data([[wandb_image, id_str]])
         # eval_artifact.add(table, 'pred_table')
         eval_artifact.add(wandb.JoinedTable(original_table, table, "id"), "joined_prediction_table")
